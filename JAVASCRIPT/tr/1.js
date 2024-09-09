eaf6dba7acb3c97c565096825cfb4dd70fb3c6e5
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2024-09-09 19:24:21 +0300

    Update 1.js

--- JAVASCRIPT/tr/1.js
+++ JAVASCRIPT/tr/1.js
@@ -316,7 +316,7 @@ observer.observe(skillsDiv);
 const disabledKeys = ["c", "C", "x", "J", "u", "I"]; // keys that will be disabled
   const showAlert = e => {
     e.preventDefault(); // preventing its default behaviour
-    return alert("Sorry, you can't view or copy source codes this way!");
+    return alert("Üzgünüz, kaynak kodlarını bu şekilde görüntüleyemez veya kopyalayamazsınız!");
   }
   document.addEventListener("contextmenu", e => {
     showAlert(e); // calling showAlert() function on mouse right click

b1b235561e6a9b8f299bcd9369adb83e1d29f5d4
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2024-10-13 09:08:36 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -33,7 +33,7 @@ const urlsToCache = [
   '/.htaccess.txt'
 ];
 
-const CURRENT_VERSION = 'v18';
+const CURRENT_VERSION = 'v19';
 
 // Install event - cache files
 self.addEventListener('install', event => {

6aaab8de04d2514eab5de33ca690884b77c12c45
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2024-11-02 20:44:00 +0300

    Update 1.js

--- JAVASCRIPT/1.js
+++ JAVASCRIPT/1.js
@@ -1,18 +1,3 @@
-/*BUTTON MOVEMENT*/
-const glassyButtons = document.querySelectorAll(".glassy-button");
-glassyButtons.forEach((button) => {
-  button.addEventListener("mousemove", (event) => {
-    const centerX = button.offsetWidth / 2;
-    const centerY = button.offsetHeight / 2;
-
-    const offsetX = event.offsetX - centerX;
-    const offsetY = event.offsetY - centerY;
-
-    button.style.setProperty("--_x-motion", `${offsetX}px`);
-    button.style.setProperty("--_y-motion", `${offsetY}px`);
-  });
-});
-
 /*IMAGE CHANGER*/
 let currentIndex = 0;
 const images = document.querySelectorAll('#photo img');

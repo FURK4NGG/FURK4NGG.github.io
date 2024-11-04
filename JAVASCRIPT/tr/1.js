23fd54fede91690297f58ba28326c37d865df7f0
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2024-11-04 21:17:29 +0300

    Update 1.js

--- JAVASCRIPT/tr/1.js
+++ JAVASCRIPT/tr/1.js
@@ -171,7 +171,7 @@ const options = {
   buttonColorDark: '#100f2c',  // default: '#100f2c'
   buttonColorLight: '#fff', // default: '#fff'
   saveInCookies: true, // default: true,
-  label: '', // default: ''
+  label: ' ', // default: ''
   autoMatchOsTheme: true // default: true
 }
 

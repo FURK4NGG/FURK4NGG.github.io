88354230c3fc2092f2b85cff50d54675c47b9d24
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2025-02-18 15:19:26 +0300

    Update 1.js

--- JAVASCRIPT/1.js
+++ JAVASCRIPT/1.js
@@ -59,7 +59,7 @@ getCurrentYear().then(year => {
     console.log(text);
 });
 
-  const text = "I am an 18-year-old university student studying Computer Engineering. Since 2020, I have mostly improved myself in back-end departments, particularly in Robotic Coding, Mobile Application Development, 2D/3D Game Development, Website Creation, Cyber Security, and AI Prompt Engineering. I have also participated in Teknofest Competitions and TUBITAK projects. Right now, I am working on projects related to AI and IoT. I am passionate about creating innovative solutions that can positively impact people’s lives and contribute to society, and I am always open to exploring new ideas that serve humanity.";
+  //const text = "I am an 18-year-old university student studying Computer Engineering. Since 2020, I have mostly improved myself in back-end departments, particularly in Robotic Coding, Mobile Application Development, 2D/3D Game Development, Website Creation, Cyber Security, and AI Prompt Engineering. I have also participated in Teknofest Competitions and TUBITAK projects. Right now, I am working on projects related to AI and IoT. I am passionate about creating innovative solutions that can positively impact people’s lives and contribute to society, and I am always open to exploring new ideas that serve humanity.";
   
   // Split text into words and wrap each word in a span
   const words = text.split(' ').map(word => {

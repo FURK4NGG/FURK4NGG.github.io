cc1e5891100543af1fea20597266f2eb6d5cb209
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2025-02-18 16:16:41 +0300

    Update 1.js

--- JAVASCRIPT/1.js
+++ JAVASCRIPT/1.js
@@ -51,6 +51,7 @@ getCurrentYear().then(year => {
     const text1 = `an ${year - 2006} years old`;
     const text2 = `I am ${text1} university student studying Computer Engineering. Since 2020, I have mostly improved myself in back-end departments, particularly in Robotic Coding, Mobile Application Development, 2D/3D Game Development, Website Creation, Cyber Security, and AI Prompt Engineering. I have also participated in Teknofest Competitions and TUBITAK projects. Right now, I am working on projects related to AI and IoT. I am passionate about creating innovative solutions that can positively impact people’s lives and contribute to society, and I am always open to exploring new ideas that serve humanity.`;
 
+  
 /* SCROLL REVEALING */
     document.addEventListener('DOMContentLoaded', () => {
         const textContainer = document.getElementById('animatedTextContainer');
@@ -71,6 +72,7 @@ getCurrentYear().then(year => {
     });
 });
 
+
 /* SCROLL EFFECT */
 document.addEventListener('scroll', () => {
     const words = document.querySelectorAll('.word');
@@ -86,7 +88,6 @@ document.addEventListener('scroll', () => {
         word.style.color = `rgba(0, 0, 0, ${opacity})`;
     });
 });
-
 
 
 /*LAZY LOADING*/

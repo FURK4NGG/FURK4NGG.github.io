e49839dbebf5fab05ca7fb1b8503cefcb0c73fd7
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2025-02-18 16:03:52 +0300

    Update 1.js

--- JAVASCRIPT/tr/1.js
+++ JAVASCRIPT/tr/1.js
@@ -119,10 +119,10 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
       .then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope);
+        //console.log('Service Worker registered with scope:', registration.scope);
       })
       .catch(error => {
-        console.log('Service Worker registration failed:', error);
+        //console.log('Service Worker registration failed:', error);
       });
   });
 }

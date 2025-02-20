72c45aea1c584e6f7b2b2b0ba8be9519aa211b70
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2025-02-20 19:01:32 +0300

    Update 1.js

--- JAVASCRIPT/1.js
+++ JAVASCRIPT/1.js
@@ -4,7 +4,7 @@ function updateThemeColor() {
     if (document.body.classList.contains("darkmode--activated")) {
         themeMetaTag.setAttribute("content", "#100f2c"); // Karanlık Mod Rengi
     } else {
-        themeMetaTag.setAttribute("content", "#fff"); // Açık Mod Rengi
+        themeMetaTag.setAttribute("content", "#000000"); // Açık Mod Rengi
     }
 }
 

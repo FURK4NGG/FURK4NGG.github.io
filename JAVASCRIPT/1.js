c79125ab28a03d2c5fe36c7a71f0ddab3229cd5b
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2025-02-24 20:01:24 +0300

    Update 1.js

--- JAVASCRIPT/1.js
+++ JAVASCRIPT/1.js
@@ -1,10 +1,12 @@
 /*CHANGE THEME*/
 let metaTheme = document.getElementById("themeMeta");
-        if (document.body.classList.contains("darkmode--activated")) {
-            metaTheme.content = "#F8F6E3";  // Light mode theme rengi
-        } else {
-            metaTheme.content = "#100f2c";  // Dark mode theme rengi
-        }
+        setTimeout(() => {
+            if (document.body.classList.contains("darkmode--activated")) {
+                metaTheme.content = "#100f2c";  // Dark mode theme rengi
+            } else {
+                metaTheme.content = "#F8F6E3";  // Light mode theme rengi
+            }
+        }, 100);
     });
 
 

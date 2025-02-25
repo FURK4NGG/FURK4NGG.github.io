b8bc84729945c6458183ffa3dc44e6e93b5f4285
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2025-02-25 11:38:24 +0300

    Update 1.js

--- JAVASCRIPT/tr/1.js
+++ JAVASCRIPT/tr/1.js
@@ -26,7 +26,7 @@ function showNotification() {
   notification.style.padding = "12px 24px";
   notification.style.borderRadius = "20px"; // Kenarları oval
   notification.style.boxShadow = "0 4px 6px rgba(0, 0, 0, 0.1)";
-  notification.style.transition = "bottom 0.5s ease-in-out";
+  notification.style.transition = "bottom 0.4s ease-in-out";
   notification.style.zIndex = "9999";
 
   document.body.appendChild(notification);

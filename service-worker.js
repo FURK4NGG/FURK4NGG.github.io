b0a8f5016360d3ff648b0aeca2c2c31564694da2
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2025-03-04 00:28:32 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
-const CURRENT_VERSION = 'v63'; // Mevcut versiyon
+const CURRENT_VERSION = 'v64';  // Versiyon numarasını güncelle
 const CACHE_NAME = `cache-${CURRENT_VERSION}`;
 
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/home_en.html',
+  '/', 
+  '/index.html', 
+  '/home_en.html', 
   '/home_tr.html',
-  '/404.html',
+  '/404.html', 
   '/CSS/unpkg.comswiper@8swiper-bundle.min.css',
-  '/FONTS/Inconsolata.woff2',
-  '/FONTS/Jaro.woff2',
+  '/FONTS/Inconsolata.woff2', 
+  '/FONTS/Jaro.woff2', 
   '/FONTS/Monomakh.woff2',
-  '/FONTS/Montserrat.woff2',
-  '/FONTS/TacOne.woff2',
+  '/FONTS/Montserrat.woff2', 
+  '/FONTS/TacOne.woff2', 
   '/FONTS/Teko.woff2',
-  '/JAVASCRIPT/1.js',
+  '/JAVASCRIPT/1.js', 
   '/JAVASCRIPT/tr/1.js',
   '/JAVASCRIPT/darkmode-js.min.js',
   '/JAVASCRIPT/gsap.min.js',
@@ -38,27 +38,44 @@ const urlsToCache = [
   '/.htaccess.txt'
 ];
 
-// Activate event - Delete all old caches and then cache the new files
+// Install event - Cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log(`Caching files for version: ${CURRENT_VERSION}`);
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate event - Delete old caches and load new ones
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          // Eski cache'leri siliyoruz
           console.log(`Deleting cache: ${cacheName}`);
           return caches.delete(cacheName);
         })
       );
     }).then(() => {
       console.log('All caches deleted');
-      // Yeni cache dosyalarını ekliyoruz
       return caches.open(CACHE_NAME).then(cache => {
-        console.log(`Caching files for version: ${CURRENT_VERSION}`);
+        console.log(`Caching new files for version: ${CURRENT_VERSION}`);
         return cache.addAll(urlsToCache);
       });
     }).then(() => {
       console.log(`Cache updated to version: ${CURRENT_VERSION}`);
-      return self.clients.claim();
+      return self.clients.claim();  // New cache activated
+    })
+  );
+});
+
+// Fetch event - Serve from cache if available, else fetch from network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });

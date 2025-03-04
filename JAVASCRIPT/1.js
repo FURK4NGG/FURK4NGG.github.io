5fe4a7a9d06f583afe4212be5cbeedd91123787d
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2025-03-05 00:53:26 +0300

    Update 1.js

--- JAVASCRIPT/1.js
+++ JAVASCRIPT/1.js
@@ -215,20 +215,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 /*SAVE DATA AND RUN OFFLINE*/
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch(error => {
-        console.log('Service Worker registration failed:', error);
-      });
-  });
-} else {
-  console.log('Service Worker is not supported in this browser.');
-}
-
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => console.log("Service Worker başarıyla yüklendi."))
+      .catch((err) => console.log("Service Worker yüklenemedi:", err));
+  }
 
 
 /*TO-TOP BUTTON REVEALING*/

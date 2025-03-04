6f521c2773312633df56fe6f9203693177ea1339
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2025-03-04 21:19:43 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,15 @@
 caches.keys().then((cacheNames) => {
     cacheNames.forEach((cacheName) => {
-        caches.open(cacheName).then((cache) => {
-            cache.keys().then((requests) => {
-                requests.forEach((request) => {
-                    if (request.url.startsWith(location.origin)) {
-                        cache.delete(request).then((deleted) => {
-                            if (deleted) {
-                                console.log(`🗑️ Silindi: ${request.url}`);
-                            }
-                        });
+        caches.open(cacheName).then(async (cache) => {
+            const requests = await cache.keys();
+            for (let request of requests) {
+                if (request.url.startsWith(location.origin)) {
+                    const deleted = await cache.delete(request);
+                    if (deleted) {
+                        console.log(`🗑️ Silindi: ${request.url}`);
                     }
-                });
-            });
+                }
+            }
         });
     });
 });

c153758e951e7d26b9b5f30801d7667fdae72c22
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2025-03-05 01:06:44 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,67 +1,22 @@
-const CACHE_NAME = 'site-cache-v1';
-const versionMap = {}; // Versiyonların yönetimi
-
-// İstenilen dosyalar burada belirtilecek
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/home_en.html',
-  '/home_tr.html',
-  '/404.html',
-  '/CSS/unpkg.comswiper@8swiper-bundle.min.css',
-  '/FONTS/Inconsolata.woff2',
-  '/FONTS/Jaro.woff2',
-  '/FONTS/Monomakh.woff2',
-  '/FONTS/Montserrat.woff2',
-  '/FONTS/TacOne.woff2',
-  '/FONTS/Teko.woff2',
-  '/JAVASCRIPT/1.js',
-  '/JAVASCRIPT/tr/1.js',
-  '/JAVASCRIPT/darkmode-js.min.js',
-  '/JAVASCRIPT/gsap.min.js',
-  '/JAVASCRIPT/swiper-bundle.min.js',
-  '/img/profile_photo1.webp',
-  '/img/character1.webp',
-  '/img/character2.webp',
-  '/img/character3.webp',
-  '/img/ai.webp',
-  '/img/app.webp',
-  '/img/cyber.webp',
-  '/img/game.webp',
-  '/img/robotic.webp',
-  '/img/web.webp',
-  '/img/up.webp',
-  '/img/icon.ico',
-  '/sitemap.xml',
-  '/manifest.json',
-  '/robots.txt',
-  '/.htaccess.txt'
-];
-
-// Install event: Cache the specified URLs
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    fetch('/version.json')
+      .then((response) => response.json())
+      .then((versionData) => {
+        // Versiyon verisini aldıktan sonra, cache'e ekle
+        const urlsToCache = Object.keys(versionData).map((file) => {
+          return `${file}?v=${versionData[file]}`;
+        });
+
+        return caches.open('v1').then((cache) => {
+          return cache.addAll(urlsToCache);
+        });
+      })
   );
 });
 
-// Fetch event: Serve cached files first, then fetch from network
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(event.request);
-    })
-  );
-});
-
-// Activate event: Cleanup old caches
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
+  const cacheWhitelist = ['v1']; // Yeni cache ismi
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
@@ -69,6 +24,23 @@ self.addEventListener('activate', (event) => {
           if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName);
           }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return (
+        cachedResponse ||
+        fetch(event.request).then((response) => {
+          const clonedResponse = response.clone();
+          caches.open('v1').then((cache) => {
+            cache.put(event.request, clonedResponse);
+          });
+          return response;
         })
       );
     })

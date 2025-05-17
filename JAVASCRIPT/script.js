bd3552013cf2718cc566b185fcfb8033f4abf902
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2025-05-17 11:13:02 +0300

    Delete JAVASCRIPT/script.js

--- JAVASCRIPT/script.js
+++
@@ -1,4 +0,0 @@
-﻿function performSearch(){let query=document.getElementById('searchQuery').value;let fileType=document.querySelector('input[name="fileType"]:checked')?.value||"";let searchPoint=document.querySelector('input[name="searchPoint"]:checked')?.value||"";let dork=`intitle:"${query}"`;if(fileType){dork+=` filetype:${fileType}`}
-if(searchPoint&&searchPoint!=="article"&&searchPoint!=="book"){dork+=` site:${searchPoint}`}else if(searchPoint==="article"){dork+=` inurl:article`}else if(searchPoint==="book"){dork+=` inurl:book`}
-window.open(`https://www.google.com/search?q=${encodeURIComponent(dork)}`);document.getElementById('searchQuery').value="";document.querySelectorAll('input[type="radio"]').forEach(input=>input.checked=!1)}
-function redirectVirusTotal(){window.open("https://www.virustotal.com/")}
